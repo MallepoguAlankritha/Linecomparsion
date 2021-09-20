2d4875324f72558e6c551babe57c77f5464c6f0b
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-20 18:41:43 +0530

    line

--- Linecomparsion.java
+++ Linecomparsion.java
@@ -11,8 +11,16 @@ public class Linecomparsion {
 		double x2 = scan.nextDouble();
 		double y1 = scan.nextDouble();
 		double y2 = scan.nextDouble();
+	    double a1 = scan.nextDouble();
+	    double a2 = scan.nextDouble();
+		double b1 = scan.nextDouble();
+		double b2 = scan.nextDouble();
+		
 		double len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		double len2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+		
 		System.out.println("length is:" + len1);
+		System.out.println("length is:" + len2);
 		}
 	}
 	
