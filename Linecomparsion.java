21257e29f668335d11765ab61769dc4c4850c2a2
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-09-20 18:35:19 +0530

    linecompare

--- Linecomparsion.java
+++ Linecomparsion.java
@@ -1,10 +1,18 @@
 package com.Alankritha;
 
+import java.util.Scanner;
+
 public class Linecomparsion {
 
 	public static void main(String[] args) {
-		System.out.println("line comparison ");
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter the values:");
+		double x1 = scan.nextDouble();
+		double x2 = scan.nextDouble();
+		double y1 = scan.nextDouble();
+		double y2 = scan.nextDouble();
+		double len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("length is:" + len1);
+		}
 	}
-
-}
+	
